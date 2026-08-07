0c832ed215a736c6dd20c4b9ef8a78add451f0f2
JonahCole <jcole8979@gmail.com>
2026-08-07 19:56:45 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='jonah-side-quests-v1';
+const CACHE='jonah-side-quests-v2';
 const CORE=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
